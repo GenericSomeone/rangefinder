59710418d79e29658366a984b78a04f922f47b22
GenericSomeone <80782836+GenericSomeone@users.noreply.github.com>
2021-05-22 12:30:12 -0700

    fixed bug where every world load would add a new rangefinder
    also added some comments because why not

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,23 @@
 function range(){
 	let raw = Mathf.dst(Vars.player.unit().x, Vars.player.unit().y, Vars.player.mouseX, Vars.player.mouseY);
+	//Math.round() rounds to the nearest integer so we need to do this to make it round to the nearest decimal instead
 	let result = (Math.round(10 * raw/8)/10);
 	return result + " tiles away";
 };
 
+//for some reason, if this doesn't exist it will add a new rangefinder every time you load the world
+let canAdd = true;
+
 Events.on(ClientLoadEvent, () => {
     Events.on(WorldLoadEvent, () => {
-	Vars.ui.hudGroup.children.get(2).row();
-        Vars.ui.hudGroup.children.get(2).label(() => range())
-            .visible(true)
-            .touchable(Touchable.disabled)
-            .name("range")
-            .top().right();
+	if(canAdd){
+		Vars.ui.hudGroup.children.get(2).row();
+	        Vars.ui.hudGroup.children.get(2).label(() => range())
+	            .visible(true)
+	            .touchable(Touchable.disabled)
+	            .name("range")
+	            .top().right();
+		canAdd = false;
+	}
     });
 });
